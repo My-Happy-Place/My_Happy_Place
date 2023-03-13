5f91f96b3f1a8c69326c522853724baa1163f63a
Daniel Silva <daniels.dev7@gmail.com>
2023-03-13 20:03:00 -0300

    Implementacao webclient para consumo de api

--- backend/src/main/java/consumer/ConsumerApi.java
+++ backend/src/main/java/consumer/ConsumerApi.java
@@ -2,12 +2,16 @@ package consumer;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class ConsumerApi {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder){
-        return null;
+        return builder.baseUrl("")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE,
+                        MediaType.APPLICATION_JSON_VALUE).build();
     }
 }
