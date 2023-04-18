5d2ec931df8de58c43a6da9bf777c3baa5cc72a4
Nicolas Josino <nicolasjosino@gmail.com>
2023-04-18 01:15:49 -0300

    Criação do componente da home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppMaterialModule } from './shared/app-material/app-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- frontend/src/app/shared/app-material/app-material.module.ts
+++ frontend/src/app/shared/app-material/app-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  exports: [MatToolbarModule, MatCardModule],
+  exports: [MatToolbarModule, MatCardModule, MatButtonModule],
 })
 export class AppMaterialModule {}
