c86219321fa6cf2d000eb19742914a5035841ae5
Nicolas Josino <nicolasjosino@gmail.com>
2023-04-20 10:46:52 -0300

    Criação do component content card

---
+++ frontend/src/app/content-card/content-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss'],
+})
+export class ContentCardComponent implements OnInit {
+  @Input() item: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
