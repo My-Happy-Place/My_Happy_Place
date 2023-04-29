dc30c906439b1343e471723ba95aa487ba82f276
Nicolas Josino <nicolasjosino@gmail.com>
2023-04-29 00:07:28 -0300

    Requisição de busca no search component

--- frontend/src/app/components/search-results/search-results.component.ts
+++ frontend/src/app/components/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
+import { ContentService } from './../../services/content/content.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
@@ -7,13 +8,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contentService: ContentService
+  ) {}
 
   search!: string;
+  results: any;
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.search = params['search'];
+    this.route.params.subscribe((routeParams) => {
+      this.search = routeParams['search'];
+
+      this.router
+        .navigateByUrl('/search', { skipLocationChange: true })
+        .then(() => {
+          this.router.navigate(['.', { myParam: this.search }]);
+        });
+
+      this.contentService.getSearchResults(this.search).subscribe((data) => {
+        this.results = data.results;
+        console.log(this.results);
+      });
     });
   }
 }
