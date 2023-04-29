7ba22c64e314b4a5e6f329ba15a32ab19e96e61b
Nicolas Josino <nicolasjosino@gmail.com>
2023-04-29 00:06:15 -0300

    Criar método getSearchResults() no content service

--- frontend/src/app/services/content/content.service.ts
+++ frontend/src/app/services/content/content.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class ContentService {
   private jsonURL = '/assets/movies.json';
+  private tmdbURL = 'https://api.themoviedb.org/3';
   private readonly apiKey = '7700f72b85d9932120594c5f27f336e1';
-  private readonly trendingURL = 'https://api.themoviedb.org/3/trending';
 
   constructor(private http: HttpClient) {}
 
@@ -18,13 +18,17 @@ export class ContentService {
 
   getTrendingMovies(): Observable<any> {
     return this.http.get(
-      `${this.trendingURL}/movie/week?api_key=${this.apiKey}&language=pt-BR`
+      `${this.tmdbURL}/trending/movie/week?api_key=${this.apiKey}&language=pt-BR`
     );
   }
 
   getTrendingTvShows(): Observable<any> {
     return this.http.get(
-      `${this.trendingURL}/tv/week?api_key=${this.apiKey}&language=pt-BR`
+      `${this.tmdbURL}/trending/tv/week?api_key=${this.apiKey}&language=pt-BR`
     );
+  }
+
+  getSearchResults(query: string): Observable<any> {
+    return this.http.get(`${this.tmdbURL}/search/multi?api_key=${this.apiKey}&language=pt-BR&query=${query}`);
   }
 }
