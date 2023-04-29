3b0c2c6d40425e0dda07dc6cc070c9734b1cfd1e
Nicolas Josino <nicolasjosino@gmail.com>
2023-04-29 00:05:37 -0300

    Melhora da searchbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ export class AppComponent {
   title = 'my-happy-place';
   logged = true; //TODO: Verificar como validar se usuário está logado
   search = '';
+
+  constructor(private router: Router){}
+
+  onEnterPress(search: string) {
+    this.router.navigate(['/search', search]);
+  }
 }
