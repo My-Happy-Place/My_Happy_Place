d1a3c0fee2eac90103dd492cb7491163594ba0c4
Nicolas Josino <nicolasjosino@gmail.com>
2023-05-25 10:21:18 -0300

    Criação de classe baseContent

--- frontend/src/app/components/content-card/content-card.component.ts
+++ frontend/src/app/components/content-card/content-card.component.ts
@@ -1,35 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { Content } from 'src/app/models/content';
-import { ContentService } from 'src/app/services/content/content.service';
+import { Component } from '@angular/core';
+import { BaseContent } from 'src/app/shared/app-material/base-content';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss'],
 })
-export class ContentCardComponent implements OnInit {
-  @Input() item!: Content;
-  releaseYear!: string;
-  readonly baseImagePath = 'https://image.tmdb.org/t/p/w400/';
-  fullImagePath!: string;
-  isFavorite!: boolean;
-  favoriteIcon!: 'favorite' | 'favorite_border';
-
-  constructor(private contentService: ContentService) {}
-
-  ngOnInit(): void {
-    this.fullImagePath = this.baseImagePath + this.item.posterPath;
-    this.releaseYear = this.item.releaseDate.substring(0, 4);
-    this.isFavorite = this.item.isFavorite;
-    this.favoriteIcon = this.isFavorite ? 'favorite' : 'favorite_border';
-  }
-
-  changeFavoriteStatus(): void {
-    this.favoriteIcon = this.isFavorite ? 'favorite_border' : 'favorite';
-    this.isFavorite = !this.isFavorite;
-    this.contentService
-      .setFavoriteStatus(this.item.idTMDB, this.item.isTvShow, this.isFavorite)
-      .subscribe();
-  }
-}
+export class ContentCardComponent extends BaseContent {}
---
+++ frontend/src/app/shared/app-material/base-content.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { Content } from 'src/app/models/content';
+import { ContentService } from 'src/app/services/content/content.service';
+
+@Component({
+  template: '',
+})
+export abstract class BaseContent {
+  @Input() item!: Content;
+  releaseYear!: string;
+  readonly baseImagePath = 'https://image.tmdb.org/t/p/w400/';
+  fullImagePath!: string;
+  isFavorite!: boolean;
+  favoriteIcon!: 'favorite' | 'favorite_border';
+
+  constructor(private contentService: ContentService) {}
+
+  ngOnInit(): void {
+    this.fullImagePath = this.baseImagePath + this.item.posterPath;
+    this.releaseYear = this.item.releaseDate.substring(0, 4);
+    this.isFavorite = this.item.isFavorite;
+    this.favoriteIcon = this.isFavorite ? 'favorite' : 'favorite_border';
+  }
+
+  changeFavoriteStatus(): void {
+    this.favoriteIcon = this.isFavorite ? 'favorite_border' : 'favorite';
+    this.isFavorite = !this.isFavorite;
+    this.contentService
+      .setFavoriteStatus(this.item.idTMDB, this.item.isTvShow, this.isFavorite)
+      .subscribe();
+  }
+}
