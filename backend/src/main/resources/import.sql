c7c7b6a0ef12f49f3eb9840c83f83265fec527d4
Daniel Silva <daniels.dev7@gmail.com>
2023-05-31 21:53:35 -0300

    Mudanca nos properties

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuario(name) VALUES ('Teste');
+INSERT INTO usuario(name) VALUES ('Tony Stark');
 INSERT INTO favorite_movies(movie_id_TMDB) VALUES (550);
 INSERT INTO favorite_series(serie_id_TMDB) VALUES (1399);
 INSERT INTO user_fav_movies(user_id, favorite_movie_id) VALUES (1,1);
